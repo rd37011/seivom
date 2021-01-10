fb8ef73069e2e19710cb205fc37b8e64f68d3378
rd37011 <rd37011@gmail.com>
2021-01-10 17:59:00 -0500

    ui overhaul

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,110 @@
 <template>
-  <div>This is the home page!</div>
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+
+      <v-icon>mdi-square</v-icon>
+
+      <v-icon>mdi-circle</v-icon>
+
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-2">
+      <v-container>
+        <v-row class="rows">
+          <template v-for="n in 4">
+            <!-- <v-col :key="n" class="mt-2" cols="auto">
+              <strong>Category {{ n }}</strong>
+            </v-col> -->
+
+            <v-col v-for="j in 4" :key="`${n}${j}`" cols="auto">
+              <v-card class="mx-auto" max-width="344">
+                <v-img
+                  src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                  height="200px"
+                ></v-img>
+
+                <v-card-title>
+                  TITLE
+                </v-card-title>
+
+                <v-card-subtitle>
+                  GENRE
+                </v-card-subtitle>
+                <v-card-subtitle>
+                  RATING
+                </v-card-subtitle>
+
+                <v-card-actions>
+                  <v-btn
+                    color="orange lighten-2"
+                    text
+                    @click.stop="detailsModalShow = true"
+                  >
+                    Explore
+                  </v-btn>
+
+                  <v-spacer></v-spacer>
+
+                  <v-btn icon @click="show = !show">
+                    <v-icon>{{
+                      show ? "mdi-chevron-up" : "mdi-chevron-down"
+                    }}</v-icon>
+                  </v-btn>
+                </v-card-actions>
+
+                <v-expand-transition>
+                  <div v-show="show">
+                    <v-divider></v-divider>
+
+                    <v-card-text>
+                      I'm a thing. But, like most politicians, he promised more
+                      than he could deliver. You won't have time for sleeping,
+                      soldier, not with all the bed making you'll be doing. Then
+                      we'll go with that data file! Hey, you add a one and two
+                      zeros to that or we walk! You're going to do his laundry?
+                      I've got to find a way to escape.
+                    </v-card-text>
+                  </div>
+                </v-expand-transition>
+              </v-card>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
+    </v-main>
+    <div ref="detailsDialog">
+      <movieDetailsModal v-model="detailsModalShow" />
+    </div>
+  </v-app>
 </template>
 <script>
+import movieDetailsModal from "./movieDetailsModal";
 export default {
-  name: "Home"
+  data: () => ({
+    show: false,
+    detailsModalShow: false,
+    drawer: null
+  }),
+  components: {
+    movieDetailsModal
+  },
+  methods: {
+    showDialog() {
+      this.$refs.detailsDialog.show();
+    }
+  }
 };
 </script>
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
---
+++ src/components/movieDetailsModal.vue
@@ -0,0 +1,111 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="show" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="Legal first name*" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  label="Legal middle name"
+                  hint="example of helper text only on focus"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Email*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Password*"
+                  type="password"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-select
+                  :items="['0-17', '18-29', '30-54', '54+']"
+                  label="Age*"
+                  required
+                ></v-select>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-autocomplete
+                  :items="[
+                    'Skiing',
+                    'Ice hockey',
+                    'Soccer',
+                    'Basketball',
+                    'Hockey',
+                    'Reading',
+                    'Writing',
+                    'Coding',
+                    'Basejump'
+                  ]"
+                  label="Interests"
+                  multiple
+                ></v-autocomplete>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click.stop="editModalShow = true">
+            Edit
+          </v-btn>
+          <v-btn color="blue darken-1" text @click.stop="show = false">
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" text @click.stop="show = false">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <div ref="editDetails">
+      <editDetailsModal v-model="editModalShow" />
+    </div>
+  </v-row>
+</template>
+<script>
+import editDetailsModal from "./editDetailsModal";
+export default {
+  name: "movieDetailsModal",
+  props: {
+    //   obj: Object,
+    value: Boolean
+  },
+  components: {
+    editDetailsModal
+  },
+  data: () => ({
+    editModalShow: false
+  }),
+  computed: {
+    show: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        return this.$emit("input", value);
+      }
+    }
+  }
+};
+</script>
+<style scoped></style>
