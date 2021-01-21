05480e9846314884bd9376f95c013c4869f4c744
rd37011 <rd37011@gmail.com>
2021-01-21 09:46:09 -0500

    remove comments

--- src/router/index.js
+++ src/router/index.js
@@ -5,31 +5,8 @@ import landingPage from "../views/landingPage.vue";
 import Register from "../views/Register";
 import store from "../store/index";
 
-// import { isInGroup } from "../auth/user";
-// import Auth from "@okta/okta-vue";
-// import { OktaAuth } from "@okta/okta-auth-js";
-
-// const config = {
-// issuer: "https://dev-8538426.okta.com/oauth2/default",
-// clientId: "0oa3vvetpjE0993sA5d6",
-// redirectUri: window.location.origin + "/login/callback",
-// scopes: ["openid", "profile", "email"]
-// }
 Vue.use(Router);
-// Vue.use(Auth, {...config});
-// const oktaAuth = new OktaAuth({
-//   issuer: "https://dev-8538426.okta.com/oauth2/default",
-//   clientId: "0oa3vvetpjE0993sA5d6",
-//   redirectUri: window.location.origin + "/login/callback",
-//   scopes: ["openid", "profile", "email"]
-// });
-// Vue.use(Auth, {
-//   issuer: 'https://dev-8538426.okta.com/oauth2/default',
-//   clientId: '0oa3vvetpjE0993sA5d6',
-//   redirectUri: 'http://localhost:8080/implicit/callback',
-//   scope: 'openid profile email'
-// })
-// Vue.use(oktaAuth);
+
 const router = new Router({
   mode: "history",
   routes: [
@@ -46,23 +23,7 @@ const router = new Router({
       name: "home",
       component: Home,
       meta: { requiresAuth: true }
-      // beforeEnter: async (to, from, next) => {
-      //   next((await isInGroup("Users")) || (await isInGroup("Admins")));
-      // }
     },
-    // {
-    //   path: "*",
-    //   beforeEnter: (to, from, next) => {
-    //     next("/");
-    //   },
-    //   meta: {
-    //     guest: true
-    //   }
-    // },
-    // {
-    //   path: "login/callback",
-    //   component: Auth.handleCallback()
-    // },
     {
       path: "/about",
       name: "About",
@@ -88,6 +49,7 @@ const router = new Router({
 
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth) && to.path !== "/") {
+    console.log("to: ", to, "from :", from, store.state.jwt === null);
     if (store.state.jwt === null) {
       next({
         path: "/",
